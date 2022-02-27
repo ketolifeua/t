c0ec03bb89668f46a43ee981fd4b54fc029c7bc4
Sergey Mostovoy <svmostovoy@gmail.com>
2022-02-27 19:28:11 +0200

    s

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 
-function get_targets {
-    setInterval(10, function() {
-
-        xhr = new XMLHttpRequest;
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                var targets = JSON.parse(xhr.responseText);
-            }
+function get_targets() {
+    xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText);
+            var targets = JSON.parse(xhr.responseText);
+            return targets;
         }
-        xhr.open("GET", 'https://www.ketolife.com.ua')
-        xhr.send();
-        return targets;
-    });
+    }
+    xhr.open("GET", 'https://raw.githubusercontent.com/ketolifeua/t/master/t.json')
+    xhr.send();
 }
 
+setInterval(1, get_targets())
